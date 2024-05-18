d405ca235e392c005f6b33f8b8628425029536b7
Abdullah <abdullah.k9913@gmail.com>
2024-05-18 10:19:04 +0500

    Updated

--- src/main/java/org/example/javafxfinalproject/Login.java
+++ src/main/java/org/example/javafxfinalproject/Login.java
@@ -11,10 +11,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 
@@ -24,7 +25,7 @@ public class Login extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Orphanage Organization Management System");
 
-        Image backgroundImage = new Image("file:///JAVAFX FINAL PROJECT/background.jpeg");
+        Image backgroundImage = new Image("file:///JAVAFX FINAL PROJECT/background2.jpg");
 
         BackgroundImage background = new BackgroundImage(
                 backgroundImage,
@@ -40,67 +41,74 @@ public class Login extends Application {
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
         grid.setBackground(backgroundWithImage);
 
         Rectangle overlay = new Rectangle();
-        overlay.setWidth(800);
-        overlay.setHeight(600);
-        overlay.setFill(Color.rgb(0,0,0,0.5));
+        overlay.setWidth(600);
+        overlay.setHeight(500);
+        overlay.setFill(Color.rgb(255,255,255,0.25));
 
-        Text scenetitle = new Text("WELCOME TO ORPHANAGE ORGANIZATION MANAGEMENT SYSTEM");
-        scenetitle.setFont(Font.font("Times New Roman", FontWeight.BOLD, 20));
-        grid.add(scenetitle, 0, 0, 2, 1);
+        Text welcomeText = new Text("WELCOME TO OOMS");
+        welcomeText.setFont(Font.font("TIMES NEW ROMAN", 30));
+        welcomeText.setTextAlignment(TextAlignment.JUSTIFY);
 
-        Label userName = new Label("Username:");
-        userName.setFont(Font.font("Tahoma", 15));
-        userName.setStyle("-fx-text-fill: black");
-        grid.add(userName, 0, 1);
+        VBox vbox = new VBox(20);
+        vbox.setAlignment(Pos.TOP_CENTER);
+        vbox.setPadding(new Insets(20, 0, 0, 0));
+        vbox.getChildren().add(welcomeText);
 
-        TextField userTextField = new TextField();
-        userTextField.setPromptText("Enter Username");
-        userTextField.setStyle("-fx-background-color: transparent;-fx-border-radius: 20px; -fx-border-color: black; -fx-text-fill: white; -fx-prompt-text-fill: black");
-        grid.add(userTextField, 1, 1);
+        HBox usernameBox = new HBox(10);
+        usernameBox.setAlignment(Pos.CENTER);
+        Label usernameLabel = new Label("Username:");
+        usernameLabel.setStyle("-fx-font: Verdana; -fx-font-size: 15; -fx-text-fill: white");
+        TextField usernameField = new TextField();
+        usernameField.setStyle("-fx-background-color: transparent; -fx-background-radius: 30px; -fx-border-color: black; -fx-border-radius: 40px; -fx-text-fill: white");
+        usernameBox.getChildren().addAll(usernameLabel, usernameField);
 
-        Label pw = new Label("Password:");
-        pw.setFont(Font.font("Tahoma", 15));
-        pw.setStyle("-fx-text-fill: black");
-        grid.add(pw, 0, 2);
+        HBox passwordBox = new HBox(10);
+        passwordBox.setAlignment(Pos.CENTER);
+        Label passwordLabel = new Label("Password:");
+        passwordLabel.setStyle("-fx-font: Verdana; -fx-font-size: 15; -fx-text-fill: white");
+        PasswordField passwordField = new PasswordField();
+        passwordField.setStyle("-fx-background-color: transparent; -fx-background-radius: 30px; -fx-border-color: black; -fx-border-radius: 40px; -fx-text-fill: white");
+        passwordBox.getChildren().addAll(passwordLabel, passwordField);
 
-        PasswordField passwordBox = new PasswordField();
-        passwordBox.setPromptText("Enter Password");
-        passwordBox.setStyle("-fx-background-color: transparent; -fx-border-radius: 20px; -fx-border-color: black; -fx-text-fill: white; -fx-prompt-text-fill: black");
-        grid.add(passwordBox, 1, 2);
+        vbox.getChildren().addAll(usernameBox, passwordBox);
 
-        Button forgotPassword = new Button("Forgot Password?");
-        forgotPassword.setStyle("-fx-border-color: black; -fx-border-radius: 40px; -fx-background-color: green; -fx-text-fill: white; -fx-background-radius: 30px");
-
-        HBox fpbtn = new HBox(10);
-        fpbtn.setAlignment(Pos.BOTTOM_LEFT);
-        fpbtn.getChildren().add(forgotPassword);
-        grid.add(fpbtn, 1, 4);
-
+        HBox buttonBox = new HBox(80);
+        buttonBox.setAlignment(Pos.CENTER);
+        buttonBox.setPadding(new Insets(20, 0, 0, 0));
+        Button forgotPasswordButton = new Button("Forgot Password?");
+        forgotPasswordButton.setStyle("-fx-background-color: purple; -fx-background-radius: 50px; -fx-border-color: black; -fx-border-radius: 10px; -fx-text-fill: white");
         Button loginButton = new Button("Login");
-        loginButton.setStyle("-fx-border-radius: 40px; -fx-border-color: black; -fx-background-color: green; -fx-text-fill: white; -fx-background-radius: 30px");
+        loginButton.setStyle("-fx-background-color: purple; -fx-background-radius: 50px; -fx-border-color: black; -fx-border-radius: 10px; -fx-text-fill: white");
+        buttonBox.getChildren().addAll(forgotPasswordButton, loginButton);
 
-        HBox lgBtn = new HBox(10);
-        lgBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        lgBtn.getChildren().add(loginButton);
-        grid.add(lgBtn, 1, 4);
+        vbox.getChildren().add(buttonBox);
 
-        Button signupButton = new Button("Sign Up");
-        signupButton.setStyle("-fx-text-fill: white; -fx-background-color: green;");
+        HBox signUpBox = new HBox(10);
+        signUpBox.setAlignment(Pos.BOTTOM_RIGHT);
+        signUpBox.setPadding(new Insets(20));
+        Text signUpText = new Text("Don't have an account?");
+        signUpText.setStyle("-fx-font-size: 15; -fx-font: Verdana;");
+        signUpText.setFill(Paint.valueOf("white"));
+        Button signUpButton = new Button("Sign Up");
+        signUpButton.setStyle("-fx-background-color: purple; -fx-background-radius: 50px; -fx-border-color: black; -fx-border-radius: 10px; -fx-text-fill: white");
+        signUpBox.getChildren().addAll(signUpText, signUpButton);
 
-        Label signUplabel = new Label("Don't have an account?");
-        signUplabel.setAlignment(Pos.BOTTOM_RIGHT);
-        signUplabel.setFont(Font.font("Tahoma", 15));
-        signUplabel.setStyle("-fx-text-fill: black; -fx-background-radius: 30px; -fx-background-color: transparent;");
+        StackPane overlayPane = new StackPane();
+        overlayPane.setMaxWidth(600);
+        overlayPane.setMaxHeight(500);
+        overlayPane.getChildren().addAll(overlay, vbox);
 
-        HBox signupBtn = new HBox(10);
-        signupBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        signupBtn.getChildren().addAll(signUplabel, signupButton);
-        grid.add(signUplabel, 1, 20);
-        grid.add(signupBtn, 1, 20);
+        StackPane bottomRightPane = new StackPane();
+        bottomRightPane.getChildren().add(signUpBox);
+        StackPane.setAlignment(signUpBox, Pos.BOTTOM_RIGHT);
+
+        StackPane mainPane = new StackPane();
+        mainPane.getChildren().addAll(overlayPane, bottomRightPane);
+
+        grid.add(mainPane, 0, 0);
 
         Scene scene = new Scene(grid, 800, 600);
         primaryStage.setScene(scene);
